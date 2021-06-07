b1ed70c707086ea1dd167b990a6515c2053b7f9d
Antonio Sebastiano <sebastianoa@hotmail.com>
2021-06-07 12:29:55 +1000

    exercises completed

--- booklist.js
+++ booklist.js
@@ -3,28 +3,35 @@ class BookManager {
 // constructor with a paramater currentId set to 0 by default
   constructor(currentId = 0) {
       // initialize an empty array called books
-    
+        this._books = [];
       //initialize a currentId set to currentId
+      this.currentId = currentId;
   }
 
   // function addBook creates an object and pushes it to our books array
   // should take in a name, genre and author and by default have an empty string
-  addBook() {
+  addBook(name, genre, author = "") {
       // create an object for the newBook
-      
+      const newBook = {
+          id: this.currentId++,
+          name: name,
+          genre: genre,
+          author: author
+      };
           // your object should consist of properties id, name,genre and author with values added when we call this method
           // don't forget that this may require some parameters to be used as values
           // id should be unique and increment by 1 every new instance
           // if no argument is given, the properties should equal an empty string
 
       // push the newly created object to our books array
+      this.books.push(newBook);
       // hint: best practice would be to access our books array using the get method
       
   }
 
   // function getter should return the value of our books array
   get books() {
-      
+      return this._books;
   }
 
 }
@@ -37,4 +44,8 @@ class BookManager {
 
 // console log our books array to check that the new books were added
 
+const bookList = new BookManager();
 
+bookList.addBook( "The Alchemist", "Life", "Paulo Coelho");
+bookList.addBook( "Time to Play", "Children's Book", "Bluey");
+console.log(bookList.books);
--- exercises.js
+++ exercises.js
@@ -1,35 +1,67 @@
 // Objects 
 
 // 1. Create an object literal ‘person’ that contains the keys firstname, lastname, place of birth and country - give them some related values
+const person ={
+	firstname: "Antonio",
+	lastname: "Sebastiano",
+	"place of birth": "Naples",
+	country: "Italy"
+}
 //  a) Console.log the object ‘person’
+//console.log(person);
 //  b) Console.log the country property of the object ‘person’
+//.log(person.country);
 //  c) Console.log the ‘place of birth’ property of the ‘person’ object
+//console.log(person["place of birth"]);
+
+
 
 // 2. Create an object in Javascript named ‘school’ that contains the keys ‘name’,’location’,’established year’ - give them some related values
+const school = {
+	name: "Generation",
+	location: "Sydney",
+	"established year": 2018,
+	displayInfo(){
+		console.log(`My school ${this.name} was established in ${this["established year"]} at ${this.location}`);
+	}
+}
 // a) Console.log the school name and location
+//console.log(school.name,school.location);
 // b) Console.log the established year
+//console.log(school["established year"]);
 // c) To that ‘school’ object, add a function ‘displayInfo’ to log to the console “My school ____ was established in ___ at ____”
 //filling the blanks with the relevant values - Do the above using the ‘this’ keyword
+//school.displayInfo();
 // d) Call the function ‘displayInfo’
 // e) Mutate the school object to add a boolean value called graduated and set it to true or false.
+//school.graduated = true;
 // f) Console.log the school object
-
+//console.log(school);
 // Classes
 // Create a class called person
 class Person {
 	constructor(name) {
-		this.name = name;
+		this._name = name;
 	}
-	getName() {
-		return this.name;
+	get name() {
+		return this._name;
 	}
 
 // 1. create a static method for creating an Anonymous person
 //  a) this should give a name variable as John Doe or Jane Doe depending on a gender parameter (hint: use if else or ternary opertors)
 //  b) it should then return a new instance of person with this name
-	
+	static anonymousPers(gender){
+		let name;
+		if(gender === "male"){
+			name = "John Doe";
+		}else if((gender === "female")) {
+			name = "Jane Doe";
+		}
+		return new Person(name);
+	}
 }
 
 // 2. use the static method and save it to a variable and console log it
 
-
+const newPerson = Person.anonymousPers("male");
+console.log(newPerson); 
